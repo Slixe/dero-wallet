98ce83468150c2c47c96e902986a3e11e39e901d
Slixe <slixeprivate@gmail.com>
2020-02-08 15:55:16 +0100

    ask password

--- src/views/Recover.vue
+++ src/views/Recover.vue
@@ -1,21 +1,34 @@
 <template>
 <div id="main">
     <h1 class="title-page">RECOVER WALLET</h1>
-    <v-card class="menu">
+    <v-card v-if="!askPassword" class="menu">
         <v-alert v-show="alertShow" :type="alertType">
             {{ alertMessage }}
         </v-alert>
         <h2>Available Wallets</h2>
         <v-select v-model="walletSelected" :items="availableWallets" item-text="name" :color="$selectColor" :item-color="$selectColor" class="wallet-select"></v-select>
         <div class="select-buttons">
-            <v-btn color="success" class="select-button" @click="openWallet()" outlined>Open Selected Wallet</v-btn>
+            <v-btn color="success" class="select-button" @click="askWalletPassword()" outlined>Open Selected Wallet</v-btn>
             <v-btn color="error" class="select-button" @click="deleteWallet()" outlined>Delete Selected Wallet</v-btn>
         </div>
         <v-divider class="div-space"></v-divider>
         <div class="recover-buttons">
-            <v-btn outlined v-for="(button, key) in recoverButtons" :key="key" class="recover-button">{{ button }}</v-btn>
+            <v-btn outlined v-for="(button, key) in recoverButtons" :key="key"  @click="wip()" class="recover-button">{{ button }}</v-btn>
         </div>
-        <v-btn outlined class="recover-view-wallet">Recover View Only Wallet</v-btn>
+        <v-btn outlined @click="wip()" class="recover-view-wallet">Recover View Only Wallet</v-btn>
+    </v-card>
+    <v-card v-else class="menu">
+        <v-alert v-show="alertShow" :type="alertType">
+            {{ alertMessage }}
+        </v-alert>
+        <h2 style="margin-bottom: 2%;">Password to open {{ walletSelected }}</h2>
+        <div id="password">
+            <v-text-field type="password" v-model="password" label="Password" :color="$selectColor" filled :disabled="btnDisabled"></v-text-field>
+            <div class="select-buttons">
+                <v-btn outlined class="recover-button" @click="cancelPassword()" :disabled="btnDisabled">Cancel</v-btn>
+                <v-btn outlined class="recover-button" @click="openWallet()" :disabled="btnDisabled">Open Wallet</v-btn>
+            </div>
+        </div>
     </v-card>
 </div>
 </template>
@@ -31,18 +44,31 @@ export default {
             recoverButtons: ["Recover using Seed", "Recover using Recovery Key", "Recover using File"],
             alertType: "error",
             alertShow: false,
-            alertMessage: "An error as occured!"
+            alertMessage: "An error as occured!",
+            askPassword: false,
+            password: "",
+            btnDisabled: false
         }
     },
     mounted() {
         this.availableWallets = wallet.getEncryptedWallets()
     },
     methods: {
+        askWalletPassword() {
+            if (this.walletSelected != null)
+            {
+                this.askPassword = true
+            }
+        },
+        cancelPassword() {
+            this.askPassword = false
+        },
         openWallet() {
             if (this.walletSelected != null)
             {
+                this.btnDisabled = true
                 let walletDump = wallet.getEncryptedWallet(this.walletSelected)
-                if (wallet.openEncryptedWallet(this.walletSelected, prompt("Password for this wallet:"), walletDump)) {
+                if (wallet.openEncryptedWallet(this.walletSelected, this.password, walletDump)) {
                     this.alertType = "success"
                     this.alertMessage = "Wallet successfully opened!"
                     this.alertShow = true
@@ -53,7 +79,7 @@ export default {
                 } else {
                     this.alertMessage = "Incorrect password for this wallet !"
                     this.alertShow = true
-
+                    this.btnDisabled = false
                     setTimeout(() => this.alertShow = false, 5000) //after 5s
                 }
             }
@@ -61,9 +87,19 @@ export default {
         deleteWallet() {
            if (this.walletSelected != null)
            {
-               wallet.removeEncryptedWallet(this.walletSelected)
-               this.availableWallets = wallet.getEncryptedWallets()
+                wallet.removeEncryptedWallet(this.walletSelected)
+                this.availableWallets = wallet.getEncryptedWallets()
+                this.alertType = "success"
+                this.alertMessage = "Wallet successfully deleted!"
+                this.alertShow = true
+                setTimeout(() => this.alertShow = false, 2000) //2s
            } 
+        },
+        wip() {
+            this.alertType = "error"
+            this.alertMessage = "Not implemented yet!"
+            this.alertShow = true
+            setTimeout(() => this.alertShow = false, 2000) //2s   
         }
     }
 }
@@ -111,6 +147,13 @@ export default {
     margin: 5%;
     margin-left: 15%;
     margin-right: 15%;
+}
+
+#password {
+    margin-left: 15%;
+    margin-right: 15%;
+    margin-top: 5%;
+    margin-bottom: 5%;
 }
 
 @media screen and (max-width: 960px)
